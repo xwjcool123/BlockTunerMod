de10da6934ca54d6b21d0690fb14a53b424f51ec
xwjcool123 <30473133+xwjcool123@users.noreply.github.com>
2021-08-23 17:31:33 +0800

    Keyboard to Piano

--- src/main/java/cool/xwj/blocktuner/BlockTunerClient.java
+++ src/main/java/cool/xwj/blocktuner/BlockTunerClient.java
@@ -9,9 +9,19 @@ import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 @Environment(EnvType.CLIENT)
 public class BlockTunerClient implements ClientModInitializer {
 
+    private static boolean keyToPiano = false;
+
     @Override
     public void onInitializeClient() {
         ScreenRegistry.register(BlockTuner.TUNING_SCREEN_HANDLER, TuningScreen::new);
 
+    }
+
+    public static boolean isKeyToPiano() {
+        return keyToPiano;
+    }
+
+    public static void toggleKeyToPiano() {
+        keyToPiano = !keyToPiano;
     }
 }
--- src/main/java/cool/xwj/blocktuner/TuningScreen.java
+++ src/main/java/cool/xwj/blocktuner/TuningScreen.java
@@ -23,6 +23,7 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
     TuningScreenHandler screenHandler;
     private BlockPos pos = null;
     private PianoKeyWidget pressedKey = null;
+    private final PianoKeyWidget[] pianoKeys = new PianoKeyWidget[25];
 
     private static final Identifier TEXTURE = new Identifier("blocktuner", "textures/gui/container/tune.png");
 
@@ -37,31 +38,33 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
 
         // Fancy(?) keyboard
 
-        this.addButton(new WhiteKeyWidget(this.x+18, this.y+44, 1, 3));
-        this.addButton(new WhiteKeyWidget(this.x+28, this.y+44, 3, 4));
-        this.addButton(new WhiteKeyWidget(this.x+38, this.y+44, 5, 5));
-        this.addButton(new WhiteKeyWidget(this.x+48, this.y+44, 6, 1));
-        this.addButton(new WhiteKeyWidget(this.x+58, this.y+44, 8, 2));
-        this.addButton(new WhiteKeyWidget(this.x+68, this.y+44, 10, 5));
-        this.addButton(new WhiteKeyWidget(this.x+78, this.y+44, 11, 1));
-        this.addButton(new WhiteKeyWidget(this.x+88, this.y+44, 13, 3));
-        this.addButton(new WhiteKeyWidget(this.x+98, this.y+44, 15, 4));
-        this.addButton(new WhiteKeyWidget(this.x+108, this.y+44, 17, 5));
-        this.addButton(new WhiteKeyWidget(this.x+118, this.y+44, 18, 1));
-        this.addButton(new WhiteKeyWidget(this.x+128, this.y+44, 20, 2));
-        this.addButton(new WhiteKeyWidget(this.x+138, this.y+44, 22, 5));
-        this.addButton(new WhiteKeyWidget(this.x+148, this.y+44, 23, 1));
-        this.addButton(new BlackKeyWidget(this.x+12, this.y+28, 0));
-        this.addButton(new BlackKeyWidget(this.x+23, this.y+28, 2));
-        this.addButton(new BlackKeyWidget(this.x+34, this.y+28, 4));
-        this.addButton(new BlackKeyWidget(this.x+52, this.y+28, 7));
-        this.addButton(new BlackKeyWidget(this.x+64, this.y+28, 9));
-        this.addButton(new BlackKeyWidget(this.x+82, this.y+28, 12));
-        this.addButton(new BlackKeyWidget(this.x+93, this.y+28, 14));
-        this.addButton(new BlackKeyWidget(this.x+104, this.y+28, 16));
-        this.addButton(new BlackKeyWidget(this.x+122, this.y+28, 19));
-        this.addButton(new BlackKeyWidget(this.x+134, this.y+28, 21));
-        this.addButton(new BlackKeyWidget(this.x+152, this.y+28, 24));
+        this.addButton(new WhiteKeyWidget(this.x + 18, this.y + 48, 1, 3));
+        this.addButton(new WhiteKeyWidget(this.x + 28, this.y + 48, 3, 4));
+        this.addButton(new WhiteKeyWidget(this.x + 38, this.y + 48, 5, 5));
+        this.addButton(new WhiteKeyWidget(this.x + 48, this.y + 48, 6, 1));
+        this.addButton(new WhiteKeyWidget(this.x + 58, this.y + 48, 8, 2));
+        this.addButton(new WhiteKeyWidget(this.x + 68, this.y + 48, 10, 5));
+        this.addButton(new WhiteKeyWidget(this.x + 78, this.y + 48, 11, 1));
+        this.addButton(new WhiteKeyWidget(this.x + 88, this.y + 48, 13, 3));
+        this.addButton(new WhiteKeyWidget(this.x + 98, this.y + 48, 15, 4));
+        this.addButton(new WhiteKeyWidget(this.x + 108, this.y + 48, 17, 5));
+        this.addButton(new WhiteKeyWidget(this.x + 118, this.y + 48, 18, 1));
+        this.addButton(new WhiteKeyWidget(this.x + 128, this.y + 48, 20, 2));
+        this.addButton(new WhiteKeyWidget(this.x + 138, this.y + 48, 22, 5));
+        this.addButton(new WhiteKeyWidget(this.x + 148, this.y + 48, 23, 1));
+        this.addButton(new BlackKeyWidget(this.x + 12, this.y + 32, 0));
+        this.addButton(new BlackKeyWidget(this.x + 23, this.y + 32, 2));
+        this.addButton(new BlackKeyWidget(this.x + 34, this.y + 32, 4));
+        this.addButton(new BlackKeyWidget(this.x + 52, this.y + 32, 7));
+        this.addButton(new BlackKeyWidget(this.x + 64, this.y + 32, 9));
+        this.addButton(new BlackKeyWidget(this.x + 82, this.y + 32, 12));
+        this.addButton(new BlackKeyWidget(this.x + 93, this.y + 32, 14));
+        this.addButton(new BlackKeyWidget(this.x + 104, this.y + 32, 16));
+        this.addButton(new BlackKeyWidget(this.x + 122, this.y + 32, 19));
+        this.addButton(new BlackKeyWidget(this.x + 134, this.y + 32, 21));
+        this.addButton(new BlackKeyWidget(this.x + 152, this.y + 32, 24));
+
+        this.addButton(new keyToPianoToggle(this.x + 142, this.y + 7));
 
     }
 
@@ -75,6 +78,7 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        assert this.client != null;
         this.client.getTextureManager().bindTexture(TEXTURE);
         int i = (this.width - this.backgroundWidth) / 2;
         int j = (this.height - this.backgroundHeight) / 2;
@@ -83,16 +87,21 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
 
     @Environment(EnvType.CLIENT)
     abstract class PianoKeyWidget extends ClickableWidget {
-        protected int note;
+        private final int note;
         protected boolean played;
 
         protected PianoKeyWidget(int x, int y, int note) {
             super(x, y, 9, 16, LiteralText.EMPTY);
             this.note = note;
+            pianoKeys[note] = this;
         }
 
         @Override
         public void onClick(double mouseX, double mouseY){
+
+            if (pressedKey != null) {
+                pressedKey.onRelease(mouseX, mouseY);
+            }
 
             pressedKey = this;
             played = true;
@@ -107,6 +116,8 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
 
             ClientPlayNetworking.send(BlockTuner.TUNE_PACKET, buf);
 
+            assert client != null;
+            assert client.player != null;
             client.player.swingHand(Hand.MAIN_HAND);
 
             if (hasControlDown()){
@@ -118,6 +129,7 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
         @Override
         public void onRelease(double mouseX, double mouseY) {
             played = false;
+            pressedKey = null;
         }
 
         @Override
@@ -159,7 +171,7 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
     }
 
     class WhiteKeyWidget extends PianoKeyWidget{
-        private int keyType;
+        private final int keyType;
 
         public WhiteKeyWidget(int x, int y, int note, int keyType) {
             super(x, y, note);
@@ -183,17 +195,160 @@ public class TuningScreen extends HandledScreen<ScreenHandler> {
 
     }
 
+    static class keyToPianoToggle extends ClickableWidget{
+        public keyToPianoToggle(int x, int y) {
+            super(x, y, 12, 9, LiteralText.EMPTY);
+        }
+
+        @Override
+        public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+            MinecraftClient.getInstance().getTextureManager().bindTexture(TEXTURE);
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+
+            int status = 0;
+            if (BlockTunerClient.isKeyToPiano()) {
+                status = 2;
+            }
+            if (this.isHovered()) {
+                status += 1;
+            }
+
+            this.drawTexture(matrices, this.x, this.y, 240, 16 * status, 11, 11);
+        }
+
+        @Override
+        public void onClick(double mouseX, double mouseY){
+            BlockTunerClient.toggleKeyToPiano();
+        }
+
+    }
+
     @Override
     public boolean mouseReleased(double mouseX, double mouseY, int button) {
 
         this.setDragging(false);
         if (pressedKey != null) {
             return pressedKey.mouseReleased(mouseX, mouseY, button);
-        }
-        else {
+        } else {
             return super.mouseReleased(mouseX, mouseY, button);
         }
 
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        // something may be here in the future.
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers){
+
+        if (BlockTunerClient.isKeyToPiano() && keyCode != 256) {
+            int note = keyToNote(scanCode);
+            if (note >= 0 && note <= 24 && (pressedKey == null || pressedKey.note != note)) {
+                pianoKeys[note].onClick(0, 0);
+            }
+
+            return true;
+        } else {
+            return super.keyPressed(keyCode, scanCode, modifiers);
+        }
+    }
+
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers){
+        if (pressedKey != null && keyToNote(scanCode) == pressedKey.note) {
+            pressedKey.onRelease(0, 0);
+        }
+        return super.keyReleased(keyCode, scanCode, modifiers);
+    }
+
+    protected int keyToNote(int scanCode) {
+        int note = -1;
+        switch (scanCode) {
+            case 3 :
+            case 38:
+                note = 7;
+                break;
+            case 4 :
+            case 39 :
+                note = 9;
+                break;
+            case 6 :
+                note = 12;
+                break;
+            case 7 :
+                note = 14;
+                break;
+            case 8 :
+                note = 16;
+                break;
+            case 10 :
+                note = 19;
+                break;
+            case 11 :
+                note = 21;
+                break;
+            case 13 :
+                note = 24;
+                break;
+            case 16 :
+            case 51 :
+                note = 6;
+                break;
+            case 17 :
+            case 52 :
+                note = 8;
+                break;
+            case 18 :
+            case 53 :
+                note = 10;
+                break;
+            case 19 :
+                note = 11;
+                break;
+            case 20 :
+                note = 13;
+                break;
+            case 21 :
+                note = 15;
+                break;
+            case 22 :
+                note = 17;
+                break;
+            case 23 :
+                note = 18;
+                break;
+            case 24 :
+                note = 20;
+                break;
+            case 25 :
+                note = 22;
+                break;
+            case 26 :
+                note = 23;
+                break;
+            case 34 :
+                note = 0;
+                break;
+            case 35 :
+                note = 2;
+                break;
+            case 36 :
+                note = 4;
+                break;
+            case 48 :
+                note = 1;
+                break;
+            case 49 :
+                note = 3;
+                break;
+            case 50 :
+                note = 5;
+                break;
+        }
+        return note;
     }
 
 }
