75d205dd1fb0515fda86a44bf7579d76357bd6ab
xwjcool123 <30473133+xwjcool123@users.noreply.github.com>
2021-09-01 16:32:13 +0800

    Play mode polyphony support
    Happy playing chords!

--- src/main/java/cool/xwj/blocktuner/BlockTuner.java
+++ src/main/java/cool/xwj/blocktuner/BlockTuner.java
@@ -5,12 +5,14 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.NoteBlock;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-
-//import static net.fabricmc.fabric.impl.networking.NetworkingImpl.MOD_ID;
+import net.minecraft.world.World;
 
 public class BlockTuner implements ModInitializer {
 
@@ -31,13 +33,22 @@ public class BlockTuner implements ModInitializer {
 
             int note = buf.readByte();
             BlockPos pos = buf.readBlockPos();
+            World world = player.world;
 
             server.execute(() -> {
 //                System.out.println("Tuned " + pos + " to " + note);
-                if (player.world.getBlockState(pos).getBlock() == Blocks.NOTE_BLOCK) {
-                    player.world.setBlockState(pos, player.world.getBlockState(pos).with(NoteBlock.NOTE, note), 3);
-                    if (player.world.getBlockState(pos.up()).isAir()) {
-                        player.world.addSyncedBlockEvent(pos, Blocks.NOTE_BLOCK, 0, 0);
+
+                if (world.getBlockState(pos).getBlock() == Blocks.NOTE_BLOCK) {
+
+                    world.setBlockState(pos, world.getBlockState(pos).with(NoteBlock.NOTE, note), 3);
+
+                    if (world.getBlockState(pos.up()).isAir()) {
+
+//                        world.addSyncedBlockEvent(pos, Blocks.NOTE_BLOCK, 0, 0);
+                        float pitch = (float)Math.pow(2.0D, (double)(note - 12) / 12.0D);
+                        world.playSound(null, pos, (world.getBlockState(pos).get(NoteBlock.INSTRUMENT)).getSound(), SoundCategory.RECORDS, 3.0F, pitch);
+                        ((ServerWorld)world).spawnParticles(ParticleTypes.NOTE, pos.getX() + 0.5D, pos.getY() + 1.2D, pos.getZ() + 0.5D, 0, (double)note / 24.0D, 0.0D, 0.0D, 1.0D);
+
                     }
 
                     player.swingHand(Hand.MAIN_HAND);
